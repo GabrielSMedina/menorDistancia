ad47cab38b8a30fd322900268e39cc147127385a
Gabriel Medina <gabriel.medina.fis@gmail.com>
2022-03-06 15:29:39 -0300

    Algoritmo funcionando, faltando comentarios e read.me

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,6 @@
 import random
+from math import sqrt
+import matplotlib.pyplot as plt
 
 
 def gerar_matriz(tamanho):
@@ -14,9 +16,42 @@ def gerar_matriz(tamanho):
     return matriz
 
 
-def calcula_dist():
-    pass
+def calcula_dist(matriz, ponto):
+    dist = None
+    X = 0
+    Y = 0
+    for c in range(len(matriz)):
+        x = matriz[c][0] - ponto[0]
+        y = matriz[c][1] - ponto[1]
+        hipo = sqrt(x ** 2 + y ** 2)
+        if dist is None:
+            dist = hipo
+
+        if hipo < dist:
+            dist = hipo
+            X = matriz[c][0]
+            Y = matriz[c][1]
+    menor_ponto = [X, Y]
+    print(menor_ponto)
+    return dist, menor_ponto
 
 
-def gera_grafico():
-    pass
+def gera_grafico(matriz, menor_ponto, ponto):
+    x = []
+    y = []
+
+    for c in range(len(matriz)):
+        x.append(matriz[c][0])
+        y.append(matriz[c][1])
+
+    A = [menor_ponto[0], ponto[0]]
+    B = [menor_ponto[1], ponto[1]]
+    # Pontos aleatorios
+    plt.scatter(x, y, color='blue')
+    # Ponto de menor distancia
+    plt.scatter(menor_ponto[0], menor_ponto[1], color='red')
+    # Ponto arbitrario
+    plt.scatter(ponto[0], ponto[1], color='green')
+    # Plot da linha
+    plt.plot(A, B, color='black')
+    plt.show()
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import funcoes
 
+
 if __name__ == '__main__':
+    # Gera uma matriz aleatoria
     matriz = funcoes.gerar_matriz(100)
+
+    # Mostra essa matriz
     print(matriz)
 
+    # Define um ponto arbitrario
+    ponto = [24, 52]
+
+    # 
+    distancia, menor_ponto = funcoes.calcula_dist(matriz, ponto)
+    distancia = round(distancia, 2)
+    print(f'A menor distancia vale {distancia}, e seu ponto no grafico cartesiano é {menor_ponto}')
+
+    funcoes.gera_grafico(matriz, menor_ponto, ponto)
 
